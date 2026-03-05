6b053398a3b245637652d761077601b40234ec23
hüseyin <130411876+huseyinsnr1@users.noreply.github.com>
2026-03-05 15:40:02 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:09:29.295Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T12:40:01.860Z
+Commit: Patch API endpoint error handling
