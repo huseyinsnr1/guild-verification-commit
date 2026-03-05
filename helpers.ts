a0f9aa441cd6236febb5c1b2808101642b30cb3f
hüseyin <130411876+huseyinsnr1@users.noreply.github.com>
2026-03-05 15:06:11 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:05:33.315Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T12:06:16.755Z
+Commit: Fix broken links in documentation
