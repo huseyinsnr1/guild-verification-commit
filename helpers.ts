82a302f015e157a21f830a2b1aae0ed06197596a
hüseyin <130411876+huseyinsnr1@users.noreply.github.com>
2026-03-05 15:06:38 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:06:30.259Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T12:06:43.988Z
+Commit: Fix broken links in documentation
