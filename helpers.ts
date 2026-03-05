08be385064290dc83279ed303ce28cda79c47763
hüseyin <130411876+huseyinsnr1@users.noreply.github.com>
2026-03-05 15:40:07 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:40:01.860Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T12:40:07.124Z
+Commit: Correct typo in configuration file
